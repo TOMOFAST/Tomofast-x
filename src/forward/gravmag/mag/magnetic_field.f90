0e266a970b537ff81c730ec899dc3f3ce03b6510
Vitaliy Ogarko <vogarko@gmail.com>
2022-01-31 02:05:11 -0800

    Replaced mbox with sharmbox.

--- src/forward/gravmag/mag/magnetic_field.f90
+++ src/forward/gravmag/mag/magnetic_field.f90
@@ -12,189 +12,62 @@
 !
 !========================================================================
 
-!========================================================================================
-! A class to calculate sensitivity values for magnetic field.
-!
-! Vitaliy Ogarko, UWA, CET, Australia, 2015-2016.
-!========================================================================================
+!========================================================================
+! Custom Tomofast-x module for computation of total magnetic field.
+! This module only computes the magnetic tensor which
+! can be used to calculate the total field at any point
+! outside the magnetizing volume.
+!========================================================================
 module magnetic_field
-
   use global_typedefs
   use grid
 
   implicit none
-
   private
 
-  ! The vacuum magnetic permeability (SI).
-  double precision, parameter :: mu0 = PI * 4.d-7
-
-  ! Degrees to radians conversion factor.
+  ! Degree to radian.
   double precision, parameter :: d2rad = PI / 180.d0
 
   ! Main class.
   type, public :: t_magnetic_field
     private
 
-    ! For optimization of mbox() function calculation precompute these parameters.
-    double precision :: fm1, fm2, fm3, fm4, fm5, fm6
-
-    ! External/background (average regional) field intensity (nT).
+    ! Ebxternal field intensity in nT.
     double precision :: intensity
 
-  contains
-    private
+    ! Magnetic field vectors.
+    double precision, dimension(3) :: magv
 
-    procedure, public, pass :: initialize => magnetic_field_initialize
-    procedure, public, pass :: magprism => magnetic_field_magprism
+    contains
+      private
 
-    procedure, private, pass :: mbox
-    procedure, private, nopass :: dircos
+      procedure, public, pass     :: initialize => magnetic_field_initialize
+      procedure, public, pass     :: magprism => magnetic_field_magprism
 
-  end type t_magnetic_field
+      procedure, private, nopass  :: sharmbox
+      procedure, private, nopass  :: dircos
+
+    end type t_magnetic_field
 
 contains
 
 !==============================================================================
-! Initialization.
-! See parameters description in mbox() function.
+! Initializes the magnetic field by calculating and storing their direction
+! cosines.
+! Currently the ambient magnetic field components are not used
 !==============================================================================
 subroutine magnetic_field_initialize(this, mi, md, fi, fd, theta, intensity)
-  class(t_magnetic_field), intent(inout) :: this
-  double precision, intent(in) :: mi, md, fi, fd, theta, intensity
+    class(t_magnetic_field), intent(inout) :: this
+    double precision, intent(in) :: mi, md, fi, fd, theta, intensity
 
-  double precision :: ma, mb, mc
-  double precision :: fa, fb, fc
+    double precision :: ma, mb, mc
 
-  this%intensity = intensity
+    this%intensity = intensity
+    call this%dircos(mi, md, theta, ma, mb, mc)
 
-  call this%dircos(mi, md, theta, ma, mb, mc)
-  call this%dircos(fi, fd, theta, fa, fb, fc)
-
-  this%fm1 = ma * fb + mb * fa
-  this%fm2 = ma * fc + mc * fa
-  this%fm3 = mb * fc + mc * fb
-  this%fm4 = ma * fa
-  this%fm5 = mb * fb
-  this%fm6 = mc * fc
+    this%magv = (/ ma, mb, mc /)
 
 end subroutine magnetic_field_initialize
-
-!====================================================================================================
-! Calculates the sensitivity kernel.
-!====================================================================================================
-subroutine magnetic_field_magprism(this, nelements, data_j, grid, Xdata, Ydata, Zdata, sensit_line)
-  class(t_magnetic_field), intent(in) :: this
-  integer, intent(in) :: nelements, data_j
-  type(t_grid), intent(in) :: grid
-  real(kind=CUSTOM_REAL), intent(in) :: Xdata(:), Ydata(:), Zdata(:)
-
-  real(kind=CUSTOM_REAL), intent(out) :: sensit_line(:)
-
-  integer :: i
-  real(kind=CUSTOM_REAL) :: t1, t2
-
-  do i = 1, nelements
-
-    call this%mbox(Xdata(data_j), Ydata(data_j), Zdata(data_j), &
-                   grid%X1(i), grid%Y1(i), grid%Z1(i), grid%X2(i), grid%Y2(i), t1)
-
-    call this%mbox(Xdata(data_j), Ydata(data_j), Zdata(data_j), &
-                   grid%X1(i), grid%Y1(i), grid%Z2(i), grid%X2(i), grid%Y2(i), t2)
-
-    sensit_line(i) = (t1 - t2) / (4.d0 * PI)
-
-  enddo
-
-end subroutine magnetic_field_magprism
-
-!===================================================================================
-!  Subroutine MBOX computes the total field anomaly of an infinitely
-!  extended rectangular prism. Sides of prism are parallel to x,y,z
-!  axes, and z is vertical down. Bottom of prism extends to infinity.
-!  Two calls to mbox can provide the anomaly of a prism with finite
-!  thickness; e.g.,
-!
-!     call mbox(x0,y0,z0,x1,y1,z1,x2,y2,mi,md,fi,fd,m,theta,t1)
-!     call mbox(x0,y0,z0,x1,y1,z2,x2,y2,mi,md,fi,fd,m,theta,t2)
-!     t=t1-t2
-!
-!  Requires subroutine DIRCOS.
-!  Method from Ref. [1]. For more details see also Refs. [2-3].
-!  [1] B. K. Bhattacharyya, Magnetic anomalies due to prism-shaped bodies with arbitrary polarization, Geophysics, 29, 1964.
-!  [2] B. K. Bhattacharyya, Computer modeling in gravity and magnetic interpretation, Geophysics, 43, 1978.
-!  [3] B. K. Bhattacharyya, A generalized multibody model for inversion of magnetic anomalies, Geophysics, 45, 1980.
-!
-!  Input parameters:
-!    Observation point is (x0,y0,z0). Prism extends from x1 to
-!    x2, y1 to y2, and z1 to infinity in x, y, and z directions,
-!    respectively.  Magnetization defined by inclination mi,
-!    declination md, intensity m.  Ambient field defined by
-!    inclination fi and declination fd.  X axis has declination
-!    theta. Distance units are irrelevant but must be consistent.
-!    Angles are in degrees, with inclinations positive below
-!    horizontal and declinations positive east of true north.
-!    Magnetization in A/m.
-!
-!  Output parameters:
-!    Total field anomaly t, in nT.
-!===================================================================================
-subroutine mbox(this, x0, y0, z0, x1, y1, z1, x2, y2, t)
-  class(t_magnetic_field), intent(in) :: this
-  double precision, intent(in) :: x0, y0, z0, x1, y1, z1, x2, y2
-  double precision, intent(out) :: t
-
-  ! Local variables.
-  double precision :: alpha(2), beta(2)
-  double precision :: alphabeta, alphasq, arg1, arg2, arg3, arg4
-  double precision :: h, hsq, r0, r0h, r0sq, sign, tatan, tlog
-  integer :: i, j
-
-  alpha(1) = x1 - x0
-  alpha(2) = x2 - x0
-
-  beta(1) = y1 - y0
-  beta(2) = y2 - y0
-
-  h = z1 - z0
-
-  ! Sanity check.
-  if (h < 0.d0) then
-    print *, "Error in mbox: negative depth h =", h
-    stop
-  endif
-
-  t = 0.
-  hsq = h**2
-
-  do i = 1, 2
-    alphasq = alpha(i)**2
-    do j = 1, 2
-      sign = 1.
-      if (i /= j) sign = -1.
-
-      r0sq = alphasq + beta(j)**2 + hsq
-      r0 = dsqrt(r0sq)
-      r0h = r0 * h
-      alphabeta = alpha(i) * beta(j)
-
-      arg1 = (r0 - alpha(i)) / (r0 + alpha(i))
-      arg2 = (r0 - beta(j)) / (r0 + beta(j))
-      arg3 = alphasq + r0h + hsq
-      arg4 = r0sq + r0h - alphasq
-
-      tlog = this%fm3 * dlog(arg1) / 2. + this%fm2 * dlog(arg2) / 2. - this%fm1 * dlog(r0 + h)
-      tatan = - this%fm4 * datan2(alphabeta, arg3) - this%fm5 * datan2(alphabeta, arg4) + this%fm6 * datan2(alphabeta, r0h)
-
-      t = t + sign * (tlog + tatan)
-    enddo
-  enddo
-
-  !t = t * mu0 * this%intensity
-  t = t * this%intensity
-
-end subroutine mbox
-
 
 !==============================================================================
 !  Subroutine DIRCOS computes direction cosines from inclination
@@ -206,22 +79,235 @@ end subroutine mbox
 !    azim:  azimuth of x axis in degrees positive east of north.
 !
 !  Output parameters:
-!    a,b,c:  the three direction cosines.
+!    a, b, c:  the three direction cosines.
 !==============================================================================
 subroutine dircos(incl, decl, azim, a, b, c)
-  double precision, intent(in) :: incl, decl, azim
-  double precision, intent(out) :: a, b, c
+    double precision, intent(in)    :: incl, decl, azim
+    double precision, intent(out)   :: a, b, c
 
-  double precision :: xincl, xdecl, xazim
+    double precision :: xincl, xdecl, xazim
 
-  xincl = incl * d2rad
-  xdecl = decl * d2rad
-  xazim = azim * d2rad
+    xincl = incl * d2rad
+    xdecl = decl * d2rad
+    xazim = azim * d2rad
 
-  a = dcos(xincl) * dcos(xdecl - xazim)
-  b = dcos(xincl) * dsin(xdecl - xazim)
-  c = dsin(xincl)
+    a = dcos(xincl) * dcos(xdecl - xazim)
+    b = dcos(xincl) * dsin(xdecl - xazim)
+    c = dsin(xincl)
 
 end subroutine dircos
+
+!==============================================================================
+! Calculates the magnetic tensor for each point and is returned to the calling process
+! This subroutine is meant to perform mbox on a set of voxels before returning their
+! respective magnetic tensor flattened in vector form.
+!==============================================================================
+subroutine magnetic_field_magprism(this, nelements, data_j, grid, Xdata, Ydata, Zdata, sensit_line)
+    class(t_magnetic_field), intent(in)     :: this
+    integer, intent(in)                     :: nelements, data_j
+    type(t_grid), intent(in)                :: grid
+    real(kind=CUSTOM_REAL), intent(in)      :: Xdata(:), Ydata(:), Zdata(:)
+
+    real(kind=CUSTOM_REAL), intent(out)     :: sensit_line(:)
+
+    integer             :: i, j
+    double precision    :: tx(3), ty(3), tz(3)
+    double precision    :: mx, my, mz
+
+    do i = 1, nelements
+        tx = 0.d0
+        ty = 0.d0
+        tz = 0.d0
+        mx = 0.d0
+        my = 0.d0
+        mz = 0.d0
+
+        call this%sharmbox(Xdata(data_j), Ydata(data_j), Zdata(data_j), &
+                           grid%X1(i), grid%Y1(i), grid%Z1(i), &
+                           grid%X2(i), grid%Y2(i), grid%Z2(i), &
+                           tx, ty, tz)
+
+    ! could be probably more efficient using fortran's native matrix multiplication support
+        do j = 1, 3
+            mx = mx + tx(j) * this%intensity * this%magv(j)
+            my = my + ty(j) * this%intensity * this%magv(j)
+            mz = mz + tz(j) * this%intensity * this%magv(j)
+        end do
+
+        sensit_line(i) = my * this%magv(1) + mx * this%magv(2) + mz * this%magv(3)
+    end do
+
+    sensit_line = sensit_line / (4.d0 * PI)
+
+end subroutine magnetic_field_magprism
+
+!===================================================================================
+! Rewritten MBOX to use the algorithm proposed by P. Vallabh Sharma in his 1966 paper
+! [Rapid Computation of Magnetic Anomalies and Demagnetization Effects Caused by Arbitrary Shape]
+! Requires the DIRCOS subroutine.
+!
+! units:
+!   coordinates:        m
+!   field intensity:    nT
+!   incl/decl/azi:      deg
+!   mag suscept.:       cgs
+!
+! inputs:
+!   x0, y0, z0      coordinates of the observation point
+!   x1, y1, z1      coordinates of one of the corners on the top face, where z1 is the depth
+!   x2, y2, z2      coordinates of the opposite corner on the bottom face, where z2 is the depth
+!
+! outputs:
+!   tx = [txx txy txz]
+!   ty = [tyx tyy tyz]
+!   tz = [tzx tzy tzz]
+!   components of the magnetic tensor
+!===================================================================================
+subroutine sharmbox(y0,x0,z0, y1,x1,z1, y2,x2,z2, ts_y,ts_x,ts_z)
+    double precision, intent(in)            :: x0,y0,z0, x1,y1,z1, x2,y2,z2
+
+    double precision, intent(out)           :: ts_x(3), ts_y(3), ts_z(3)
+
+    ! local
+    double precision :: rx1, rx2,        ry1, ry2,        rz1, rz2
+    double precision :: rx1sq, rx2sq,    ry1sq, ry2sq,    rz1sq, rz2sq
+    double precision :: arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8
+    double precision :: R1, R2, R3, R4
+    double precision :: dx, dy, dz, eps = 1.d-8
+    logical          :: l_inside
+
+    l_inside = .false.
+
+    ! relative coordinates to obs
+    ! voxel runs from x1 to x2, y1 to y2, z1 to z2
+    ! observation points at obs_x obs_y obs_z
+    rx1 = x1 - x0 + eps; ! rx1 = -u2
+    rx2 = x2 - x0 + eps; ! rx2 = -u1
+    ry1 = y1 - y0 + eps; ! ry1 = -v2
+    ry2 = y2 - y0 + eps; ! ry2 = -v1
+    rz1 = z1 - z0 + eps; ! rz1 = -w2
+    rz2 = z2 - z0 + eps; ! rz2 = -w1
+
+    ! squares
+    rx1sq = rx1 ** 2; rx2sq = rx2 ** 2;
+    ry1sq = ry1 ** 2; ry2sq = ry2 ** 2;
+    rz1sq = rz1 ** 2; rz2sq = rz2 ** 2;
+
+    R1 = ry2sq + rx2sq ! -v1**2 + -u1**2 -> R1
+    R2 = ry2sq + rx1sq ! -v1**2 + -u2**2 -> R3
+    R3 = ry1sq + rx2sq ! -v2**2 + -u1**2 -> R2
+    R4 = ry1sq + rx1sq ! -v2**2 + -u2**2 -> R4
+    arg1 = SQRT(rz2sq + R2)
+    arg2 = SQRT(rz2sq + R1)
+    arg3 = SQRT(rz1sq + R1)
+    arg4 = SQRT(rz1sq + R2)
+    arg5 = SQRT(rz2sq + R3)
+    arg6 = SQRT(rz2sq + R4)
+    arg7 = SQRT(rz1sq + R4)
+    arg8 = SQRT(rz1sq + R3)
+
+    ! ts_xx
+    ts_x(1) =   DATAN2(ry1 * rz2, (rx2 * arg5 + eps)) - &
+                DATAN2(ry2 * rz2, (rx2 * arg2 + eps)) + &
+                DATAN2(ry2 * rz1, (rx2 * arg3 + eps)) - &
+                DATAN2(ry1 * rz1, (rx2 * arg8 + eps)) + &
+                DATAN2(ry2 * rz2, (rx1 * arg1 + eps)) - &
+                DATAN2(ry1 * rz2, (rx1 * arg6 + eps)) + &
+                DATAN2(ry1 * rz1, (rx1 * arg7 + eps)) - &
+                DATAN2(ry2 * rz1, (rx1 * arg4 + eps))
+
+    ! ts_yx
+    ts_y(1) =   DLOG((rz2 + arg2 + eps) / (rz1 + arg3 + eps)) - &
+                DLOG((rz2 + arg1 + eps) / (rz1 + arg4 + eps)) + &
+                DLOG((rz2 + arg6 + eps) / (rz1 + arg7 + eps)) - &
+                DLOG((rz2 + arg5 + eps) / (rz1 + arg8 + eps))
+
+    ! ts_yy
+    ts_y(2) =   DATAN2(rx1 * rz2, (ry2 * arg1 + eps)) - &
+                DATAN2(rx2 * rz2, (ry2 * arg2 + eps)) + &
+                DATAN2(rx2 * rz1, (ry2 * arg3 + eps)) - &
+                DATAN2(rx1 * rz1, (ry2 * arg4 + eps)) + &
+                DATAN2(rx2 * rz2, (ry1 * arg5 + eps)) - &
+                DATAN2(rx1 * rz2, (ry1 * arg6 + eps)) + &
+                DATAN2(rx1 * rz1, (ry1 * arg7 + eps)) - &
+                DATAN2(rx2 * rz1, (ry1 * arg8 + eps))
+
+    ! following computations do not reuse variables so it may be
+    ! faster to just compute them directly instead of storing
+    ! them. It does help legibility, however
+    R1 = ry2sq + rz1sq
+    R2 = ry2sq + rz2sq
+    R3 = ry1sq + rz1sq
+    R4 = ry1sq + rz2sq
+    arg1 = SQRT(rx1sq + R1)
+    arg2 = SQRT(rx2sq + R1)
+    arg3 = SQRT(rx1sq + R2)
+    arg4 = SQRT(rx2sq + R2)
+    arg5 = SQRT(rx1sq + R3)
+    arg6 = SQRT(rx2sq + R3)
+    arg7 = SQRT(rx1sq + R4)
+    arg8 = SQRT(rx2sq + R4)
+
+    ! ts_yz
+    ts_y(3) =   DLOG((rx1 + arg1 + eps) / (rx2 + arg2 + eps)) - &
+                DLOG((rx1 + arg3 + eps) / (rx2 + arg4 + eps)) + &
+                DLOG((rx1 + arg7 + eps) / (rx2 + arg8 + eps)) - &
+                DLOG((rx1 + arg5 + eps) / (rx2 + arg6 + eps))
+
+    R1 = rx2sq + rz1sq
+    R2 = rx2sq + rz2sq
+    R3 = rx1sq + rz1sq
+    R4 = rx1sq + rz2sq
+    arg1 = SQRT(ry1sq + R1)
+    arg2 = SQRT(ry2sq + R1)
+    arg3 = SQRT(ry1sq + R2)
+    arg4 = SQRT(ry2sq + R2)
+    arg5 = SQRT(ry1sq + R3)
+    arg6 = SQRT(ry2sq + R3)
+    arg7 = SQRT(ry1sq + R4)
+    arg8 = SQRT(ry2sq + R4)
+
+    ! ts_xz
+    ts_x(3) =   DLOG((ry1 + arg1 + eps) / (ry2 + arg2 + eps)) - &
+                DLOG((ry1 + arg3 + eps) / (ry2 + arg4 + eps)) + &
+                DLOG((ry1 + arg7 + eps) / (ry2 + arg8 + eps)) - &
+                DLOG((ry1 + arg5 + eps) / (ry2 + arg6 + eps))
+
+    ! checking if point is inside the voxel
+    ! if so, use poisson's relation
+    dx = x2 - x1
+    dy = y2 - y1
+    dz = z2 - z1
+    if (x0 > min(x1,x2) .and. x0 < max(x1,x2)) then
+        if (y0 > min(y1,y2) .and. y0 < max(y1,y2)) then
+            if (-z0 > min(-z1,-z2) .and. -z0 < max(-z1,-z2)) then
+                l_inside = .true.
+            end if
+        end if
+    end if
+
+
+    ! filling the rest of the tensor
+    ! ts_zz
+    if (l_inside) then
+        print *, "Observation point inside target voxel!"
+        print *, "obs:", x0,y0,z0
+        print *, "voxel:", x1,x2, y1,y2, z1,z2
+
+        ts_z(3) = -1 * (ts_x(1) + ts_y(2) + 4*PI) ! poisson
+    else
+        ts_z(3) = -1 * (ts_x(1) + ts_y(2)) ! gauss
+    end if
+
+    ! ts_zy
+    ts_z(2) = ts_y(3)
+
+    ! ts_xy
+    ts_x(2) = ts_y(1)
+
+    ! ts_zx
+    ts_z(1) = ts_x(3)
+
+end subroutine sharmbox
 
 end module magnetic_field
